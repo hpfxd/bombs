e7b2bb5a765cf0ee10675ce33cae32631e23140b
hpf <nathan@rape.lol>
2018-06-03 06:17:03 -0400

    fix errors

--- src/main/java/xyz/tooger/bombs/Bombs.java
+++ src/main/java/xyz/tooger/bombs/Bombs.java
@@ -4,12 +4,19 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Bombs extends JavaPlugin {
+	private static Bombs instance;
+
     public void onEnable() {
+    	this.instance = this;
         this.getServer().getPluginManager().registerEvents(new BombListener(), this);
         this.getConfig().options().copyDefaults(true);
     }
 
     static FileConfiguration getCfg() {
-        return new Bombs().getConfig();
+        return getInstance().getConfig();
+    }
+
+    public static Bombs getInstance() {
+    	return instance;
     }
 }
